4ef3d1976863aa514a093d3c2716cf515aa468e9
rohitbisht01 <rb9519208@gmail.com>
2025-03-23 19:52:59 +0530

    useFetch commented

--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-const useFetch = (cb) => {
-  const [data, setData] = useState(undefined);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
+// const useFetch = (cb) => {
+//   const [data, setData] = useState(undefined);
+//   const [loading, setLoading] = useState(null);
+//   const [error, setError] = useState(null);
 
-  return <div>useFetch</div>;
-};
+//   return <div>useFetch</div>;
+// };
 
-export default useFetch;
+// export default useFetch;
